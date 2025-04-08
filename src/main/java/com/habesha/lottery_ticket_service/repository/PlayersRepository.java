59241e5b24f81ac9dc6389c80b01665fa2353ce8
elenimw <elenimulugetaw@gmail.com>
2025-04-08 17:51:13 -0500

    LOT-5 Database set up

--- src/main/java/com/habesha/lottery_ticket_service/repository/PlayersRepository.java
+++ src/main/java/com/habesha/lottery_ticket_service/repository/PlayersRepository.java
@@ -1,11 +1,11 @@
 package com.habesha.lottery_ticket_service.repository;
-import com.habesha.lottery_ticket_service.model.Players;
+import com.habesha.lottery_ticket_service.dao.PlayersDao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
-public interface PlayerRepository extends JpaRepository<Players, UUID> {
+public interface PlayersRepository extends JpaRepository<PlayersDao, UUID> {
     //methods
-    Players findByEmail(String email);
+    PlayersDao findByEmail(String email);
 
 
 
